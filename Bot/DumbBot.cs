f3a998067c42d9b2a8716b66ada70ad0376cb89e
ThierryCols <thierrycolsenet@gmail.com>
2015-01-30 10:16:36 +0100

    Bot Abstraction
    creating dumb bot class & modifying some abstract methods as virtual
    ones.

---
+++ Bot/DumbBot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Engine;
+using Models.Map;
+
+namespace Bot
+{
+    public class DumbBot : AbstractBot
+    {
+        public DumbBot(IClient client) : base(client)
+        {
+            
+        }
+        
+        public override void onMapInitialization(object sender, MapUpdateEventArgs mapInitializationEventArgs)
+        {
+            
+
+        }
+        public override void onMapUpdate(object sender, MapUpdateEventArgs mapUpdateEventsArgs)
+        {
+
+
+        }
+        public override void onHomeSet(object sender, MapUpdateEventArgs homeSetEventArgs)
+        {
+
+
+        }
+        public override void onHousesSet(object sender, MapUpdateEventArgs housesSetEventArgs)
+        {
+
+
+        }
+        public override void onGameEnd(object sender, EventArgs eventArgs)
+        {
+
+
+        }
+        public override void onDisconnection(object sender, EventArgs eventArgs)
+        {
+
+
+        }
+
+    }
+}
