c0e38a9e333d047e7e3438a34e191f8fa7ceb3f5
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2015-02-06 10:18:18 +0100

    Add hash related methods to AbstractMap

--- Maps/AbstractMap.cs
+++ Maps/AbstractMap.cs
@@ -8,14 +8,27 @@ namespace Kate.Maps
     [Serializable()]
     public abstract class AbstractMap: IMap
     {
+        private int hash = 0;
+
         public abstract IEnumerable<Tile> getGrid();		
         public abstract void setTile(Tile newTile);
         public abstract Tile getTile (int xCoordinate, int yCoordinate);
 		public abstract IEnumerable<Tile> getPlayerTiles(Owner owner);
         public abstract int[] getMapDimension();
 
-        public virtual void updateMap (IMapUpdater mapUpdater)
+        public abstract void ComputeHash();
+        public override int GetHashCode()
         {
+            if (hash == 0) ComputeHash();
+                return hash;
+        }
+        private abstract void UpdateHash(IMapUpdater mapUpdater);
+
+        public virtual void updateMap(IMapUpdater mapUpdater)
+        {
+            if (hash == 0) ComputeHash();
+                UpdateHash(mapUpdater);
+
             mapUpdater.execute(this);
         }
     }
