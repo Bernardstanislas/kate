06dcd599e8c9c895f5a7b813af785d7b0c5d00bd
Damien Frikha <damien.frikha@outlook.com>
2015-02-09 14:28:27 +0100

    Add entry point to the program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace kate
+{
+    class Program
+    {
+        public static int Main()
+        {
+            Console.WriteLine("Salut moi c'est KATE");
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
