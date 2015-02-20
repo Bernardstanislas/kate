3fa9c8d0ef6c8349b4c22bf4110fdfa75ce54f85
Damien Frikha <damien.frikha@outlook.com>
2015-02-20 16:12:03 +0100

    Add command line arguments to main
    With the config file to launch it from debug with the IDE.

--- Program.cs
+++ Program.cs
@@ -7,11 +7,27 @@ namespace kate
 {
     class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
-            Console.WriteLine("KATE is starting...");
-            var socket = new SocketClient("127.0.0.1", 5555);
-            new RandomBot(socket, "KATE");
+            string ipAddress = "";
+            int port = 0;
+
+            foreach (var arg in args)
+            {
+                var content = arg.Split('=');
+                if (content[0] == "-ip")
+                    ipAddress = content[1];
+                else if (content[0] == "-port")
+                    port = Convert.ToInt32(content[1]);
+            }
+
+            if (ipAddress == "" || port == 0)
+                throw new ArgumentException("Missing IP or port number");
+
+            Console.WriteLine("Connecting to " + ipAddress + " on port " + port.ToString());
+            var socket = new SocketClient(ipAddress, port);
+            new DumbBot(socket, "KATE");
+            Console.WriteLine("KATE has started");
             Console.ReadKey(true);
         }
     }
