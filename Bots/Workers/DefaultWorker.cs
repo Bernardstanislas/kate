174d497bf6dc9a6d268db4eb186e72efcfc62165
Stanislas Bernard <bernardstanislas@gmail.com>
2015-03-06 16:08:40 +0100

    Fix confilcts

--- Bots/Workers/DefaultWorker.cs
+++ Bots/Workers/DefaultWorker.cs
@@ -1,11 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+using Kate.Maps;
+using Kate.Types;
+using Kate.Heuristic;
+using Kate.Heuristic.Rules;
 
 namespace Kate.Bots.Workers
 {
 	public class DefaultWorker : AbstractWorker
 	{
-		public DefaultWorker(IMap map, Owner turn) : base(map, turn)
+		private static readonly HeuristicManager heuristicManager = new HeuristicManager(new Dictionary<IScoringRule, int>{
+			{new PopulationRatioRule(), 1},
+			{new TotalPopulationRule(), 1}
+		});
+
+		public DefaultWorker (IMap map, Owner turn) : base (map, turn) {}
+
+		public override List<TreeNode> computeNodeChildren()
 		{
+			List<IMap> mapPerNode = generateMapPerNode ();
+			var treeNodes = new ConcurrentBag<TreeNode> ();
+
+			Parallel.ForEach (mapPerNode, item => {
+				treeNodes.Add (new TreeNode (item, heuristicManager.getScore (item)));
+			});
+
+			return new List<TreeNode> (treeNodes);
 		}
 	}
 }
