5700b8c69c57e351566d0674aeb9dba94d1b5cc7
Stanislas Bernard <bernardstanislas@gmail.com>
2015-03-06 11:11:08 +0100

    Add empty default worker

---
+++ Bots/Workers/DefaultWorker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kate.Bots.Workers
+{
+	public class DefaultWorker : AbstractWorker
+	{
+		public DefaultWorker(IMap map, Owner turn) : base(map, turn)
+		{
+		}
+	}
+}
+
