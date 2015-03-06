855ef68c88d0348d3e1f8f5019657b6b91223585
Damien Frikha <damien.frikha@outlook.com>
2015-03-06 17:14:02 +0100

    Fixes

--- Bots/Workers/AbstractWorker.cs
+++ Bots/Workers/AbstractWorker.cs
@@ -11,37 +11,38 @@ namespace Kate.Bots.Workers
     public abstract class AbstractWorker : IWorker
     {
         public IMap Map { get; private set; }
-
         public Owner Turn { get; private set; }
 
-        protected AbstractWorker (IMap map, Owner turn)
+        protected AbstractWorker(IMap map, Owner turn)
         {
             Map = map;
             Turn = turn;
         }
 
-        public abstract List<TreeNode> ComputeNodeChildren ();
+        public abstract List<TreeNode> ComputeNodeChildren();
 
-        protected virtual List<IMap> generateMapPerNode ()
+        protected virtual List<IMap> GenerateMapPerNode()
         {
-            var mapPerNode = new List<IMap> ();
-            foreach (var mapUpdaters in getMapUpdatersPerNode()) {
-				var map = new Map((Map) Map);
-                foreach (var mapUpdater in mapUpdaters) {
-                    map.updateMap (mapUpdater);
-                }
-                mapPerNode.Add (map);
+            var mapPerNode = new List<IMap>();
+            foreach (var mapUpdaters in GetMapUpdatersPerNode()) 
+            {
+				var map = new Map((Map)Map);
+                foreach (var mapUpdater in mapUpdaters)
+                    map.updateMap(mapUpdater);
+
+                mapPerNode.Add(map);
             }
             return mapPerNode;
         }
 
-        private List<List<MapUpdater>> getMapUpdatersPerNode ()
+        private List<List<MapUpdater>> GetMapUpdatersPerNode()
         {
-            var movesListsPerNode = MoveGenerator.GenerateMoves (Map, Owner.Me);
-            var mapUpdatersPerNode = new List<List<MapUpdater>> ();
-            foreach (var moveList in movesListsPerNode) {
-                mapUpdatersPerNode.Add (MapUpdaterFactory.Generate (moveList));
-            }
+            var movesListsPerNode = MoveGenerator.GenerateMoves(Map, Turn);
+
+            var mapUpdatersPerNode = new List<List<MapUpdater>>();
+            foreach (var moveList in movesListsPerNode)
+                mapUpdatersPerNode.Add(MapUpdaterFactory.Generate(moveList));
+            
             return mapUpdatersPerNode;
         }
     }
