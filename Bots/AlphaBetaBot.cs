c0b908038132372be3020bf5e24b2ba172df8d7e
Damien Frikha <damien.frikha@outlook.com>
2015-03-16 23:34:50 +0100

    Fix AlphaBetaBot

--- Bots/AlphaBetaBot.cs
+++ Bots/AlphaBetaBot.cs
@@ -32,7 +32,8 @@ namespace Kate.Bots
             tree = new Dictionary<int, TreeNode>();
             tree.Add(map.GetHashCode(), new TreeNode(map));
 
-            var childNodes = getChildNodes(map, Owner.Me);
+            var childNodes = getChildNodeHashes(map.GetHashCode(), Owner.Me);
+
             var bestNode = tree[map.GetHashCode()];
 
             int depth = 0;
@@ -48,7 +49,7 @@ namespace Kate.Bots
                         if (newAlpha > alpha)
                         {
                             alpha = newAlpha;
-                            bestNode = child;
+                            bestNode = tree[child];
                         }
                     }
                 });
@@ -64,15 +65,16 @@ namespace Kate.Bots
             return bestNode.MoveList;
         }
 
-        private float alphaBeta(TreeNode node, int depth, float alpha, float beta, Owner player)
+        private float alphaBeta(int nodeHash, int depth, float alpha, float beta, Owner player)
         {
+            var node = tree[nodeHash];
             if (depth == 0 || node.Map.HasGameEnded())
                 return node.Heuristic;
 
             if (player == Owner.Me)
             {
-                var childNodes = getChildNodes(node.Map, Owner.Me);
-                foreach (var child in childNodes)
+                var childNodeHashes = getChildNodeHashes(nodeHash, Owner.Me);
+                foreach (var child in childNodeHashes)
                 {
                     alpha = Math.Max(alpha, alphaBeta(child, depth - 1, alpha, beta, Owner.Opponent));
                     if (beta < alpha)
@@ -82,8 +84,8 @@ namespace Kate.Bots
             }
             else
             {
-                var childNodes = getChildNodes(node.Map, Owner.Opponent);
-                foreach (var child in childNodes)
+                var childNodeHashes = getChildNodeHashes(nodeHash, Owner.Opponent);
+                foreach (var child in childNodeHashes)
                 {
                     beta = Math.Min(beta, alphaBeta(child, depth - 1, alpha, beta, Owner.Me));
                     if (beta < alpha)
@@ -93,25 +95,29 @@ namespace Kate.Bots
             }
         }
 
-        private IEnumerable<TreeNode> getChildNodes(IMap map, Owner turn)
+        private IEnumerable<int> getChildNodeHashes(int nodeHash, Owner turn)
         {
-            var parentHash = map.GetHashCode();
-            var parentChildrenHashes = tree[parentHash].GetChildrenHashes(turn);
+            var node = tree[nodeHash];
+            var existingNodeChildrenHashes = node.GetChildrenHashes(turn);
 
-            if (parentChildrenHashes.Count() > 0)
-                return parentChildrenHashes.Select(hash => tree[hash]);
-            
-            var newWorker = WorkerFactory.Build(worker, map, turn);
 
+            if (existingNodeChildrenHashes.Count() > 0)
+                return existingNodeChildrenHashes;
+
+            var newWorker = WorkerFactory.Build(worker, node.Map, turn);
             var nodeChildren = newWorker.ComputeNodeChildren();
 
             foreach (var childNode in nodeChildren)
-                if(!tree.ContainsKey(childNode.GetHashCode()))
+            {
+                if (!tree.ContainsKey(childNode.GetHashCode()))
                     tree.Add(childNode.GetHashCode(), childNode);
+            }
 
-            tree[parentHash].AddChildren(nodeChildren.Select(node => node.GetHashCode()), turn);
-            
-            return nodeChildren;
+            var nodeChildrenHashes = nodeChildren.Select(child => child.GetHashCode());
+
+            node.AddChildren(nodeChildrenHashes, turn);
+
+            return nodeChildrenHashes;
         }
     }
 }
